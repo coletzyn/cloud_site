60d8553e39ed6b9b71b1c89d32f116b4252e60fc
Cole N <77225931+coletzyn@users.noreply.github.com>
2023-10-03 15:46:02 -0500

    Update loginRoute.js

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 var path = require('path');
 
 router.get('/', (req,res) => {
-    res.sendfile(path.join(__dirname, '../', 'login.html'));
+    res.sendFile(path.join(__dirname, '../', 'login.html'));
 })
 
 router.get('/register.html', (req,res) => {
-    res.sendfile(path.join(__dirname, '../', 'register.html'));
+    res.sendFile(path.join(__dirname, '../', 'register.html'));
 
 })
 
 router.get('/success.html', (req,res) => {
-    res.sendfile(path.join(__dirname, '../', 'success.html'));
+    res.sendFile(path.join(__dirname, '../', 'success.html'));
 
 })
 
