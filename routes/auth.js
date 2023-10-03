3bc1397a7befc3d111ac41c547b86702fa95caa0
Cole N <77225931+coletzyn@users.noreply.github.com>
2023-10-03 17:55:28 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/User');
 const { KnownEmailSendStatus,EmailClient } = require("@azure/communication-email");
 require("dotenv").config();
-// This code demonstrates how to fetch your connection string
-// from an environment variable.
+
 const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
 const emailClient = new EmailClient(connectionString);
 
